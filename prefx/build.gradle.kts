a956c3384f20f6434f5e7d4d153cbc22458656dc
irgaly <irgaly@gmail.com>
2020-07-26 14:12:23 +0900

    add maven settings

--- prefx/build.gradle.kts
+++ prefx/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     kotlin("android")
+    `maven-publish`
 }
 
 android {
@@ -9,4 +10,23 @@ android {
 
 dependencies {
     implementation(Dependencies.Kotlin.stdlib)
+}
+
+val androidSourcesJar by tasks.creating(Jar::class) {
+    archiveClassifier.set("sources")
+    from(android.sourceSets["main"].java.srcDirs)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                from(components["release"])
+                artifact(androidSourcesJar)
+                groupId = "net.irgaly"
+                artifactId = "prefx"
+                version = Versions.versionName
+            }
+        }
+    }
 }
